e7e86a1baff347fcaa9025ae1baa64b28630ba12
AmberTripathi <amber.tripathi55555@gmail.com>
2025-09-09 16:48:08 +0530

    added button

--- content.js
+++ content.js
@@ -11,6 +11,17 @@ function findComposeToolbar() {
         return null;
     }
 }
+function getEmailContent() {
+    const selectors = ['.a3s.aiL','.h7','[role = "presentation"]','.gmail_quote'];
+    
+    for(const selector of selectors) {
+        const content = document.querySelector(selector);
+        if(content) {
+            return content.innerText.trim();
+        }
+        return '';
+    }
+}
 function createAIButton() {
     const button = document.createElement('div')
     button.className = 'T-I J-J5-Ji aoO v7 T-I-atl L3'
@@ -33,6 +44,40 @@ function injectButton() {
     button.classList.add("ai-reply-button");
 
     button.addEventListener('click',async()=>{
+        try {
+            button.innerHTML = 'Generating...'
+            button.disabled = true;
+
+            const emailContent = getEmailContent();
+           const response = await fetch("http://localhost:8080/api/email/generate",{method:"POST",
+                headers:{
+                    'Content-Type': 'application/json',
+                },
+                body:JSON.stringify({
+                    emailContent:emailContent,
+                    tone:"professional"
+                })
+            })
+            if(!response.ok) {
+                throw new Error('API request failed')
+            }
+            const generatedReply = await response.text();
+            const composeBox = document.querySelector('[role = "textbox"][g_editable = "true"]')
+            if(composeBox) {
+                composeBox.focus()
+                document.execCommand('insertText', false, generatedReply);
+            }
+            else {
+                console.log("Composebox not found")
+            }
+        } catch (error) {
+            console.error(error)
+            alert("Failed to generate the reply")
+        }
+        finally {
+            button.innerHTML = "AI Reply"
+            button.disabled = false
+        }
 
     });
     toolbar.insertBefore(button, toolbar.firstChild);
